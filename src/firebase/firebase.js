73a1c2e3975bd74d954f72e32f19eba20e61090c
Hajin Kim <68993476+hajineats@users.noreply.github.com>
2023-03-12 22:53:50 +1100

    initialise project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC6UyfXOvtIhgrJlfdMSh0jhSCND4wmewU",
+    authDomain: "habit-tracking-dashboard.firebaseapp.com",
+    projectId: "habit-tracking-dashboard",
+    storageBucket: "habit-tracking-dashboard.appspot.com",
+    messagingSenderId: "86411704323",
+    appId: "1:86411704323:web:abf278458c019e588e7636"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
